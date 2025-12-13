f7ae1c02e6c890174ba3053678a0601b71534357
SIMRAN YADAV <113013904+siimrrann@users.noreply.github.com>
2025-12-13 11:11:23 +0530

    Update ClassesAndObjects1.java

--- ClassesAndObjects1.java
+++ ClassesAndObjects1.java
@@ -2,12 +2,14 @@ class Movie{
     String title;
     int rating, duration;
 
+    //  parameterized constructor of the class
     Movie(String title, int rating, int duration){
         this.title = title;
         this.rating = rating;
         this.duration = duration;
     }
 
+    // a method that returns String data type 
     String getCategory(){
         if (rating >= 8){
             return "Blockbuster";
@@ -20,9 +22,13 @@ class Movie{
 
 }
 
+// the main class of the program
 public class ClassesAndObjects1{
     public static void main(String[] args) {
+
+        // creating object of the class Movie ans passing the arguments
         Movie m = new Movie("Wake Up Sid", 5, 4);
         System.out.println("The category of the movie "+m.title+ " of duration " + m.duration + " hrs is " +m.getCategory());
     }
+
 }
